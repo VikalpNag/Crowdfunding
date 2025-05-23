9a5142a0d73d031cdb0a78aa2e19a9d2ac44b468
Vikalp Nag <vikalpnag@gmail.com>
2025-05-24 01:18:17 +0530

    Add: Index.css to main.jsx to integrate tailwind

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import App from "./App";
+import App from "./App.tsx";
+import "./index.css";
 
 // Ensure 'rootElement' is non-null using type assertion
 const rootElement = document.getElementById("root") as HTMLElement;
