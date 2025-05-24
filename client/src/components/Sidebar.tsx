70e1b620845bd00a9e4f0d24ef92ba371a4e2f6e
Vikalp Nag <vikalpnag@gmail.com>
2025-05-24 12:18:49 +0530

    Add : comments for clean code

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -10,9 +10,12 @@ const Sidebar = () => {
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      {/* top icon */}
       <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
+
+      {/* mid section */}
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((link) => (
@@ -29,6 +32,8 @@ const Sidebar = () => {
             />
           ))}
         </div>
+
+        {/* bottom icon */}
         <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
       </div>
     </div>
