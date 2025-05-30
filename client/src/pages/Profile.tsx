6ec86bafd7b772a5360d81a5a75ab7ec61cd2e24
Vikalp Nag <vikalpnag@gmail.com>
2025-05-30 17:05:23 +0530

    feat : User profile with created campaigns

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const {
+    address,
+    contract,
+    getCampaigns,
+    getUserCampaigns,
+  } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="Your campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
 };
 
 export default Profile;
