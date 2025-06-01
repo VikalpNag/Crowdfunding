71446574ee8d19110607f4fa5b3388a255a847bd
Vikalp Nag <vikalpnag@gmail.com>
2025-06-02 02:12:22 +0530

    Fix: fixed all bugs and added feature fund campaign

--- client/src/context/index.tsx
+++ client/src/context/index.tsx
@@ -1,25 +1,22 @@
 import React, { useContext, createContext } from "react";
+
 import {
   useAddress,
   useContract,
-  useContractWrite,
   useMetamask,
+  useContractWrite,
 } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 
-const StateContext = createContext(null);
+const StateContext = createContext();
 
-export const StateContextProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+export const StateContextProvider = ({ children }) => {
   const { contract } = useContract(
     "0xbB8821Cb6E0D17a5Da1Db758c1cfdE9748C32c6d",
     "custom",
     { chainId: 11155111 } // Sepolia's chainId
   );
-
   const { mutateAsync: createCampaign } = useContractWrite(
     contract,
     "createCampaign"
@@ -28,27 +25,29 @@ export const StateContextProvider = ({
   const address = useAddress();
   const connect = useMetamask();
 
-  //Create campaign
   const publishCampaign = async (form) => {
     try {
-      const data = await contract.call("createCampaign", [
-        address,
-        form.title,
-        form.description,
-        form.target,
-        new Date(form.deadline).getTime(),
-        form.image,
-      ]);
+      const data = await createCampaign({
+        args: [
+          address, // owner
+          form.title, // title
+          form.description, // description
+          form.target,
+          new Date(form.deadline).getTime(), // deadline,
+          form.image,
+        ],
+      });
+
       console.log("contract call success", data);
     } catch (error) {
       console.log("contract call failure", error);
     }
   };
 
-  //get Campaign
   const getCampaigns = async () => {
     const campaigns = await contract.call("getCampaigns");
-    const parsedCampaigns = campaigns.map((campaign) => ({
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
       owner: campaign.owner,
       title: campaign.title,
       description: campaign.description,
@@ -58,9 +57,10 @@ export const StateContextProvider = ({
         campaign.amountCollected.toString()
       ),
       image: campaign.image,
+      pId: i,
     }));
 
-    return parsedCampaigns;
+    return parsedCampaings;
   };
 
   const getUserCampaigns = async () => {
@@ -69,24 +69,31 @@ export const StateContextProvider = ({
     const filteredCampaigns = allCampaigns.filter(
       (campaign) => campaign.owner === address
     );
+
     return filteredCampaigns;
   };
 
   const donate = async (pId, amount) => {
-    const data = await contract.call("donateToCampaign", pId, address, amount);
+    const data = await contract.call("donateToCampaign", [pId], {
+      value: ethers.utils.parseEther(amount),
+    });
+
     return data;
   };
 
-  const getDonation = async (pId) => {
-    const donations = await contract?.call("getDonators", pId);
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", [pId]);
     const numberOfDonations = donations[0].length;
+
     const parsedDonations = [];
+
     for (let i = 0; i < numberOfDonations; i++) {
       parsedDonations.push({
         donator: donations[0][i],
-        donation: ethers.utils.formatEther(donations[1][i].toString),
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
       });
     }
+
     return parsedDonations;
   };
 
@@ -100,7 +107,7 @@ export const StateContextProvider = ({
         getCampaigns,
         getUserCampaigns,
         donate,
-        getDonation,
+        getDonations,
       }}
     >
       {children}
--- client/src/pages/CampaignDetails.tsx
+++ client/src/pages/CampaignDetails.tsx
@@ -1,25 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
+
 import { useStateContext } from "../context";
 import { CustomButton } from "../components";
+
+import CountBox from "../components/CountBox";
 import { calculateBarPercentage, daysLeft } from "../utils";
 import { thirdweb } from "../assets";
-import { CountBox } from "../components";
 
 const CampaignDetails = () => {
   const { state } = useLocation();
-  const { getDonations, contract, address } = useStateContext();
+  const navigate = useNavigate();
+  const { donate, getDonations, contract, address } = useStateContext();
+
   const [isLoading, setIsLoading] = useState(false);
   const [amount, setAmount] = useState("");
   const [donators, setDonators] = useState([]);
+
   const remainingDays = daysLeft(state.deadline);
 
-  const handleDonate = async () => {};
+  const fetchDonators = async () => {
+    const data = await getDonations(state.pId);
+
+    setDonators(data);
+  };
+
+  useEffect(() => {
+    if (contract) fetchDonators();
+  }, [contract, address]);
+
+  const handleDonate = async () => {
+    setIsLoading(true);
+
+    await donate(state.pId, amount);
+
+    navigate("/");
+    setIsLoading(false);
+  };
 
   return (
     <div>
-      {isLoading && "...Loading"}
+      {isLoading && "<Loader />"}
+
       <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
         <div className="flex-1 flex-col">
           <img
@@ -40,21 +63,24 @@ const CampaignDetails = () => {
             ></div>
           </div>
         </div>
+
         <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
           <CountBox title="Days Left" value={remainingDays} />
           <CountBox
             title={`Raised of ${state.target}`}
             value={state.amountCollected}
           />
-          <CountBox title="Total backers" value={donators.length} />
+          <CountBox title="Total Backers" value={donators.length} />
         </div>
       </div>
-      <div className="mt-[60px] flex lg:flex-row flex-col gap-5 ">
+
+      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
         <div className="flex-[2] flex flex-col gap-[40px]">
-          <div className="">
-            <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
               Creator
             </h4>
+
             <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
               <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
                 <img
@@ -72,39 +98,59 @@ const CampaignDetails = () => {
                 </p>
               </div>
             </div>
+          </div>
+
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Story
+            </h4>
 
             <div className="mt-[20px]">
-              <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
-                Story
-              </h4>
-              <p className="mt-[2px] font-epilogue font-normal text-[14px] text-[#808191] leading-[26px] text-justify">
+              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
                 {state.description}
               </p>
             </div>
+          </div>
+
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Donators
+            </h4>
 
             <div className="mt-[20px] flex flex-col gap-4">
-              <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
-                Donators
-              </h4>
               {donators.length > 0 ? (
-                donators.map((item, index) => <div>DONATOR</div>)
+                donators.map((item, index) => (
+                  <div
+                    key={`${item.donator}-${index}`}
+                    className="flex justify-between items-center gap-4"
+                  >
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">
+                      {index + 1}. {item.donator}
+                    </p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">
+                      {item.donation}
+                    </p>
+                  </div>
+                ))
               ) : (
-                <p className="mt-[2px] font-epilogue font-normal text-[14px] text-[#808191] leading-[26px] text-justify">
-                  No donators yet.Be the first one!
+                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
+                  No donators yet. Be the first one!
                 </p>
               )}
             </div>
           </div>
         </div>
-        <div className="flex-1 ">
-          <h4 className="font-epilogue font-semibold text-[18px] text-white p-3 uppercase">
+
+        <div className="flex-1">
+          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
             Fund
           </h4>
+
           <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
-            <p className="font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]">
+            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
               Fund the campaign
             </p>
-            <div className="mt-30px">
+            <div className="mt-[30px]">
               <input
                 type="number"
                 placeholder="ETH 0.1"
@@ -113,15 +159,17 @@ const CampaignDetails = () => {
                 value={amount}
                 onChange={(e) => setAmount(e.target.value)}
               />
-              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px] ">
+
+              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
                 <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">
                   Back it because you believe in it.
                 </h4>
                 <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">
-                  Support the project for no reward,just because it speaks to
+                  Support the project for no reward, just because it speaks to
                   you.
                 </p>
               </div>
+
               <CustomButton
                 btnType="button"
                 title="Fund Campaign"
