93849a3f6a40a6989de7d81564f2e0d086daad6b
Vikalp Nag <vikalpnag@gmail.com>
2025-06-22 14:47:34 +0530

    Add: declaration file for TypeScript

---
+++ client/src/declaration.d.ts
@@ -0,0 +1,10 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
